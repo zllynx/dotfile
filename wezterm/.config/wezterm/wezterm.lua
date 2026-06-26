355cf8866b560d27f344e5b8969bb9b66c080ea6
zllynx <ztruth@qq.com>
2026-06-26 09:49:58 +0800

    feat(wezterm): add wezterm config with Ayu theme
    - Set Ayu color scheme
    - Configure JetBrainsMono Nerd Font at size 14
    - Enable tab bar and window decorations

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,1 +1,22 @@
 -- Wezterm Configuration
+
+local wezterm = require 'wezterm'
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- 主题
+-- config.color_scheme = 'Kanagawa Dragon (Gogh)'
+config.color_scheme = 'Gruvbox Material (Gogh)'
+  
+-- 字体 (Maple Mono NF CN)
+config.font = wezterm.font 'Maple Mono NF CN'
+config.font_size = 16
+
+-- 窗口
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
+
+
+return config
