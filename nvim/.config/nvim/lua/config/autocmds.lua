024d3c468da284b97c78c88f0bd0b9fee81b6238
zllynx <ztruth@qq.com>
2026-06-26 09:49:55 +0800

    config(nvim): switch to ayu colorscheme with transparent background
    - Replace nightfox with neovim-ayu colorscheme
    - Add ColorScheme autocmd for transparent Normal background
    - Remove commented dashboard-nvim config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -15,3 +15,15 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+-- 透明背景
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+  end,
+})
